76abd7c57e39f6eb0e5a94737ac39927c943da59
ShrabanSah77 <shrabanshah77@gmail.com>
2026-03-26 21:08:51 -0400

    finding error

--- mycalc/static/scripts/script.js
+++ mycalc/static/scripts/script.js
@@ -1,7 +1,24 @@
+const display = document.getElementById('display');
+
+// Add value
 function appendToResult(value) {
-    document.getElementById('display').value += value;
+    display.value += value;
+}
+// Clear
+function clearResult() {
+    display.value = '';
 }
 
-function clearResult() {
-    document.getElementById('display').value = '';
+// Backspace
+function backspace() {
+    display.value = display.value.slice(0, -1);
+}
+
+// Calculate result (for "=" button if not submitting form)
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch {
+        display.value = "Error";
+    }
 }
